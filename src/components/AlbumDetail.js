314b4d42f60b32e5d499182bd7477caba8f3d512
qi <12meridiansmedia@gmail.com>
2018-05-17 22:24:04 +0300

    Add CardSection component.

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Text } from "react-native";
 import Card from "./Card";
+import CardSection from "./CardSection";
 
 class AlbumDetail extends Component {
   render() {
@@ -8,7 +9,9 @@ class AlbumDetail extends Component {
 
     return (
       <Card>
-        <Text>{title}</Text>
+        <CardSection>
+          <Text>{title}</Text>
+        </CardSection>
       </Card>
     );
   }
