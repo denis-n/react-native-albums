dac755086dd1c2621f11f17fad65918aa53b3919
qi <12meridiansmedia@gmail.com>
2018-05-18 14:06:42 +0300

    Add button component.

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Text, View, Image, StyleSheet } from "react-native";
 import Card from "./Card";
 import CardSection from "./CardSection";
+import Button from "./Button";
 
 class AlbumDetail extends Component {
   render() {
@@ -21,8 +22,13 @@ class AlbumDetail extends Component {
             <Text>{artist}</Text>
           </View>
         </CardSection>
+
         <CardSection>
           <Image source={{ uri: image }} style={styles.imageStyle} />
+        </CardSection>
+
+        <CardSection>
+          <Button />
         </CardSection>
       </Card>
     );
